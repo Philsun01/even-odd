bea32ca14a291fc08594d6720622f01eeacbd5b5
Philip Sun <philipsun@Philips-MacBook-Pro.local>
2020-01-18 11:12:21 -0800

    added even functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
 
+const AddNumber = ({setNumbers, numbers}) => {
+
+  const [even, setEven] = useState(false);
+
+  const randomNumber = () => {
+    if(even){
+      return Math.ceil( (Math.random()*5) )*2;
+    }
+    return Math.ceil(Math.random()*10);
+  }
+
+  const onSubmit = (ev) => {
+    ev.preventDefault();
+    console.log("submitted");
+    setNumbers([...numbers, randomNumber()]);
+  }
+
+  return (
+    <form onSubmit = {onSubmit}>
+      <div>
+        <input type = 'checkbox' 
+          name = 'isEven'
+          checked = {even}
+          onChange = {(ev) => {  setEven(!even) } }/> Even Numbers Only
+      </div>
+      <button> Add New Number  </button>
+      
+    </form>
+    
+  )
+}
+
+const Status = ({numbers}) => {
+  return(
+    <div>
+      We have 
+      x
+      unique 
+    </div>
+  )
+
+}
+
+const RenderNumbers = ({numbers}) => {
+
+  return (
+    <ul>
+      {
+        numbers.map( (num, idx) => {
+          return (
+            <li key = {idx}>
+              {num}
+            </li>
+          )
+        })
+      }
+    </ul>
+    
+  )
+
+}
+
+
+
 function App() {
+
+  const [numbers, setNumbers ] = useState( [] );
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1> Even-Odd </h1>
+    <Status numbers = {numbers} />
+
+    <AddNumber setNumbers = {setNumbers} numbers = {numbers}/>
+    
+    <RenderNumbers numbers = {numbers} />
     </div>
   );
 }
